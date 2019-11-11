f5e78ae481db73f23fcc8db8cf660bb6f38d9883
BeardedBearBro <49446675+BeardedBearBro@users.noreply.github.com>
2019-11-11 18:29:05 +0100

    Add files via upload

---
+++ 20191111.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="content-type" content="text/html">
+    <title>Document</title>
+    <style>
+     @import 'style.css';
+     </style>
+</head>
+<body id="g1">
+    <aside id="g1_1">
+        <div id="g1_1_1"></div>
+        <section id="g1_1_2">
+         CSS Grid Layout is the most powerful layout system available in CSS. It is a 2-dimensional system, meaning it can handle both columns and rows, unlike flexbox which is largely a 1-dimensional system. You work with Grid Layout by applying CSS rules both to a parent element (which becomes the Grid Container) and to that elements children (which become Grid Items).</section>
+    </aside>
+    <main id="g1_2"> 
+        <header id="g1_2_1">CSS Grid Layout Challenge</header>
+        <div id="g1_2_2"></div>
+        <section id="g1_2_3">
+            <div id="g1_2_3_1"></div>
+            <div id="g1_2_3_2"></div>
+            <div id="g1_2_3_3"></div>
+        </section>
+        <footer id="g1_2_4">
+        Defines the element as a grid container and establishes a new grid formatting context for its contents.
+        Determines a grid item's location within the grid by referring to specific grid lines. grid-column-start/grid-row-start is the line where the item begins, and grid-column-end/grid-row-end is the line where the item ends.
+        </footer>
+    </main>
+</body>
+</html>
+
